2cdc78f1b953faf924e254905fbf8d653385041d
Louis Escher <louisescher@proton.me>
2025-10-04 20:21:58 +0200

    More work on homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,32 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
+import lenis from 'astro-lenis';
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://dee-znuts.live",
-	output: "static",
+	site: 'https://dee-znuts.live',
+	output: 'static',
 	experimental: {
 		fonts: [
 			{
 				provider: fontProviders.fontsource(),
-				name: "DM Serif Display",
-				cssVariable: "--font-dm-serif-display",
-				display: "swap",
-				fallbacks: ["Times New Roman"],
-				styles: ["normal", "italic"]
+				name: 'DM Serif Display',
+				cssVariable: '--font-dm-serif-display',
+				display: 'swap',
+				fallbacks: ['Times New Roman'],
+				styles: ['normal', 'italic'],
 			},
 			{
 				provider: fontProviders.fontsource(),
-				name: "DM Sans",
-				cssVariable: "--font-dm-sans",
-				display: "swap",
+				name: 'DM Sans',
+				cssVariable: '--font-dm-sans',
+				display: 'swap',
 				weights: [200, 400, 700],
-				fallbacks: ["sans-serif"],
-				styles: ["normal"]
+				fallbacks: ['sans-serif'],
+				styles: ['normal'],
 			},
-		]
-	}
+		],
+	},
+	integrations: [lenis()],
 });
